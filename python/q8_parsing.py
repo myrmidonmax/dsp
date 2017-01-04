476bf2075f3a52f854b36761c3940a514e2e739a
myrmidonmax <maxkneissl@Maxs-MBP.fritz.box>
2017-01-04 17:56:00 +0100

    Completed parsing task.

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,6 +4,22 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+
+import csv
+
+with open("football.csv") as fb_file:
+	# Reads tuples(Team and goal difference (goal_diff)) into list, sorts it according to goal_diff
+
+	football_clubs = []
+	fb_reader = csv.DictReader(fb_file)
+	for row in fb_reader:
+		football_clubs.append( (row["Team"], abs(int(row["Goals"]) - int(row["Goals Allowed"]))) )
+	football_clubs_sorted = sorted(football_clubs, key= lambda goal_diff: goal_diff[1])[0][0]
+	print(football_clubs_sorted + "is the club with the smallest absolute difference in goals.")
+
+
+
+'''
 import string
 
 def read_football():
@@ -23,6 +39,6 @@ def read_football():
 	return football_clubs_2
 
 print (read_football())
-
+'''
 
 #incomplete
