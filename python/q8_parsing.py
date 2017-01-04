02c5d3c5bb29090fa8d40b869ab0fb5f99dfb359
myrmidonmax <maxkneissl@Maxs-MacBook-Pro.local>
2017-01-04 04:11:26 +0100

    Reading csv file into list of lists.

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,25 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+import string
+
+def read_football():
+	# read football.csv file into list of tuples
+	football_clubs = []
+	football_clubs_2 = []
+	
+	fin = open("football.csv")
+	for line in fin:
+		
+		football_clubs.append(line.strip())
+	for club in football_clubs:
+		football_clubs_2.append(club.split(","))
+	football_clubs_2.pop(0)
+	for club in football_clubs_2:
+		club.append(abs(int(club[5])-int(club[6]))) 	
+	return football_clubs_2
+
+print (read_football())
+
+
+#incomplete
