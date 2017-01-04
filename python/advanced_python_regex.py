1cc5e119ca23ffcf6f675a9e92b39e809660b75e
myrmidonmax <maxkneissl@Maxs-MBP.fritz.box>
2017-01-04 23:57:52 +0100

    Part I - Q1 completed.

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,20 @@
-PLACE YOUR CODE HERE
+
+
+
+import csv, string, operator
+
+with open("faculty.csv", "r") as fac:
+	degrees = []
+	degrees_count = {}
+
+	fac_reader = csv.DictReader(fac)
+	for row in fac_reader:
+		degrees.append(row[" degree"].replace(".", "").strip().split())
+
+	for i in range(len(degrees)):
+		for j in range(len(degrees[i])):
+			if degrees[i][j] != "0":
+				degrees_count[degrees[i][j]] = degrees_count.setdefault(degrees[i][j], 0) + 1
+	print("There are %d different degrees. The frequencies are:\n" % len(degrees_count), sorted(degrees_count.items(), key = operator.itemgetter(1), reverse=True) )
+
+
